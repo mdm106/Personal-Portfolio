5d2fb831b29f666d9170d0120ffed85e15dd3110
mdm106 <mads.mcmahon@googlemail.com>
2020-05-10 11:57:33 +0100

    Gulp-rename and Gulp-sass installed, and gulpfile.js created with sass task and sass watch to convert scss to css

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+let gulp = require('gulp');
+let rename = require('gulp-rename');
+let sass = require('gulp-sass');
+
+// final tasks 
+gulp.task('sass', () => {
+    return gulp.src('./src/css/style.scss')
+      .pipe(sass())
+      .pipe(rename('style.css'))
+      .pipe(gulp.dest('./src/css/'));
+  });
+
+  gulp.task('sass-watch', () => {
+    return gulp.watch('src/css/**/*.scss', gulp.task('sass'));
+  }) // double asterisk means anything within any file under scss
